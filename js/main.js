db76a0fccbd059860d2796bf262f8fc533e965c7
dsgonzag <45009733+dsgonzag@users.noreply.github.com>
2019-07-15 20:52:47 -0500

    Drag and Drop de una imagen

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function allowDrop(ev){
+    ev.preventDefault();
+  }
+  function drag(ev){
+    ev.dataTransfer.setData("Text",ev.target.id);
+  }
+  function drop(ev){
+   ev.preventDefault();
+   var datos=ev.dataTransfer.getData("Text");
+   ev.target.appendChild(document.getElementById(datos));
+  }

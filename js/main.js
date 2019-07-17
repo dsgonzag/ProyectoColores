03b59cabaacec9aa20f06ddeafbee91e44452772
dsgonzag <45009733+dsgonzag@users.noreply.github.com>
2019-07-16 22:09:54 -0500

    Menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,44 @@
+
+
+function loadJSON(path, success, error)
+{
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function()
+    {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                if (success)
+                    success(JSON.parse(xhr.responseText));
+            } else {
+                if (error)
+                    error(xhr);
+            }
+        }
+    };
+    xhr.open("GET", path, true);
+    xhr.send();
+}
+
+loadJSON('https://api.myjson.com/bins/bjog3',
+         function(data) { 
+         console.log(data['arrayColores']);
+         for(let obj of data['arrayColores']){
+             var colores = document.createElement("img");
+             
+             var ruta = "img/"+obj['imagen'];
+             colores.setAttribute("imagen",ruta);
+        
+             var celda = document.createElement('td');
+              
+             
+            var fila = document.getElementById("imagen");
+            celda.appendChild(colores);
+             fila.appendChild(celda);
+             
+         }
+},
+         function(xhr) { console.error(xhr); }
+);
 function allowDrop(ev){
     ev.preventDefault();
   }
